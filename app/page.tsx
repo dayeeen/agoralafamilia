c936f6e22355010a757681c52d4cdcaf6e56af77
dayeeen <nubicodes@gmail.com>
2026-01-04 11:34:44 +0700

    feat: integrate AOS for scroll animations and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import HeroSection from "@/components/HeroSection";
 import AcaraSection from "@/components/AcaraSection";
 import GallerySection from "@/components/GallerySection";
@@ -7,15 +12,23 @@ import Footer from "@/components/Footer";
 import { FloatingNavbar } from "@/components/Navbar";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({ 
+      duration: 1000, 
+      once: true,
+      easing: 'ease-in-out'
+    });
+  }, []);
+
   return (
     <main className="min-h-screen px-4 md:px-[50px] pt-4 md:pt-[30px] bg-white">
       <FloatingNavbar />
-      <HeroSection />
-      <AcaraSection />
-      <GallerySection />
-      <MessageSection />
-      <RSVPSection />
-      <Footer />
+      <div data-aos="fade-up"><HeroSection /></div>
+      <div data-aos="fade-up"><AcaraSection /></div>
+      <div data-aos="fade-up"><GallerySection /></div>
+      <div data-aos="fade-up"><MessageSection /></div>
+      <div data-aos="fade-up"><RSVPSection /></div>
+      <div data-aos="fade-up"><Footer /></div>
     </main>
   );
 }
